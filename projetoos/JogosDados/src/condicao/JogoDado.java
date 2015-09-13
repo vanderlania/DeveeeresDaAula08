c227e5e65a795c6573db67847cbf2fc12b3c8e9d
vanderlania <lanaferreirads@gmail.com>
2015-09-13 15:05:19 -0300

    aula 08

---
+++ projetoos/JogosDados/src/condicao/JogoDado.java
@@ -0,0 +1,67 @@
+
+package condicao;
+
+import java.util.Scanner;
+import java.util.Scanner;
+
+
+public class JogoDado {
+     public static void main(String[] args) {
+
+        int pontos = 0;
+
+        boolean sair = false;
+        System.out.println("Digite (9) a qualquer momento para sair do jogo");
+        System.out.println("Digite (0) para reiniciar o jogo");
+
+        while (!sair) {
+            System.out.println("Você tem " + pontos + " pontos!");
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("Qual seu chute?");
+            int comando = s.nextInt();
+
+            if (9 == comando) {
+                break;
+//*
+                
+                } else if (0 ==comando){
+                   pontos = 0;
+                   
+                }
+            } else if (comando >= 1 && comando <= 6){
+                    
+                boolean resultado = jogaDado(comando);
+                if (resultado) {
+                    pontos ++;
+                  
+                }
+            } else {
+                System.out.println("Digite um numero válido");
+            }
+        }
+
+
+    }
+
+    private static boolean jogaDado(int chute) {
+
+        Random r = new Random();
+        int dado = r.nextInt(6) + 1;
+
+        System.out.println("Resultado dos dados é: " + dado);
+
+        if (dado == chute) {
+
+     System.out.println("parabens voce acertou");
+    return true;
+
+        } else {
+ System.out.println("voce errou");
+
+            return false;
+
+        } 
+    }
+    
+}
